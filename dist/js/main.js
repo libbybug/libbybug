ed5c564dd2c74c9d5e9ff7e989f1a7237f23ca24
Ainsley T <atoma4@nait.ca>
2025-11-27 11:55:58 -0700

    Delete dist directory

--- dist/js/main.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.getElementById('menuToggle');
-const menu = document.getElementById('menu');
-
-menuToggle.addEventListener('click', function() {
-    const isExpanded = menuToggle.getAttribute('aria-expanded') === 'true';
-    menuToggle.setAttribute('aria-expanded', !isExpanded);
-    menu.classList.toggle('hidden');
-});
